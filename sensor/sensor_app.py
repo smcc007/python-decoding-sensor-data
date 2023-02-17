0e5485a9a9b9154f445872ad0cf4e78880893b56
Stephen McCormick <stephen.d.mcc@gmail.com>
2023-02-17 16:37:11 -0500

    LoadSensor Data - Completed functions that filter records data by area and date.

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,5 +1,7 @@
 # Runner script for all modules
+from datetime import date, datetime
 from load_data import load_sensor_data
+from house_info import HouseInfo
 
 
 ##############################
@@ -19,3 +21,18 @@ print(f'Loaded records {len(data)}')
 # Module 4 code here:
 
 # Module 5 code here:
+
+house_info = HouseInfo(data)
+
+#TEST the area filter
+test_area = 1
+recs = house_info.get_data_by_area("id", rec_area=test_area)
+
+print(f"Number of sensor records picked up for area {test_area} is {len(recs)}")
+
+#test the date filter
+test_date = datetime.strptime("5/9/20", "%m/%d/%y")
+recs = house_info.get_data_by_date("id", rec_date=test_date)
+
+print(f"Number of sensor records picked on {test_date.strftime('%m/%d/%y')} is {len(recs)}")
+
