2292f2df70e29f7e6e2ce8b4bfa8f14c0a0c598a
Stephen McCormick <stephen.d.mcc@gmail.com>
2023-02-20 14:57:20 -0500

    Module 5 - humidity class created.

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -3,6 +3,8 @@ from datetime import date, datetime
 from load_data import load_sensor_data
 from house_info import HouseInfo
 from temperature_info import TemperatureData
+from humidity_info import HumidityData
+from statistics import mean
 
 
 ##############################
@@ -15,28 +17,23 @@ print("Sensor Data App")
 # Module 1 code here:
 data = load_sensor_data()
 print(f'Loaded records {len(data)}')
-# Module 2 code here:
-
-# Module 3 code here:
-
-# Module 4 code here:
-
-# Module 5 code here:
-
-house_info = HouseInfo(data)
-
-#TEST the area filter
-test_area = 1
-recs = house_info.get_data_by_area("id", rec_area=test_area)
-
-print(f"Number of sensor records picked up for area {test_area} is {len(recs)}")
 
 #test the date filter
 test_date = datetime.strptime("5/9/20", "%m/%d/%y")
+test_area = 1
+
+# Module 2 code here:
+
+
+
+# Module 3 code here:
+house_info = HouseInfo(data)
+recs = house_info.get_data_by_area("id", rec_area=test_area)
+print(f"Number of sensor records picked up for area {test_area} is {len(recs)}")
+
 recs = house_info.get_data_by_date("id", rec_date=test_date)
-
 print(f"Number of sensor records picked on {test_date.strftime('%m/%d/%y')} is {len(recs)}")
-
+# Module 4 code here:
 temperature_data = TemperatureData(data)
 recs = temperature_data.get_data_by_area(rec_area=test_area)
 
@@ -46,3 +43,17 @@ print(f"The Maximum temp is {max(recs)} and the minimum temp is {min(recs)}")
 recs = temperature_data.get_data_by_date(rec_date=test_date)
 print(f"The house temperature sensor records for temperature: {test_date.strftime('%m/%d/%y')} is {len(recs)}")
 print(f"The Maximum temp is {max(recs)} and the minimum temp is {min(recs)}")
+
+# Module 5 code here:
+humidity_data = HumidityData(data)
+recs = humidity_data.get_data_by_area(rec_area=test_area)
+
+print(f"The humidity sensor has picked up {len(recs)} for a test area of {test_area}")
+print(f"The average humidity is {mean(recs)}")
+
+recs = humidity_data.get_data_by_date(rec_date=test_date)
+print(f"The humidity sensor has picked up {len(recs)} for a test date of {test_date.strftime('%m/%d/%y')}")
+print(f"The average humidity is {mean(recs)}")
+
+
+
