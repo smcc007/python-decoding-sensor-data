cdf3555c5215e505000ec7714c2e3fb9ee242fcd
Stephen McCormick <stephen.d.mcc@gmail.com>
2023-02-20 14:18:59 -0500

    Module 4 - Temp class implementation as child class of HouseInfo.

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -2,6 +2,7 @@
 from datetime import date, datetime
 from load_data import load_sensor_data
 from house_info import HouseInfo
+from temperature_info import TemperatureData
 
 
 ##############################
@@ -36,3 +37,12 @@ recs = house_info.get_data_by_date("id", rec_date=test_date)
 
 print(f"Number of sensor records picked on {test_date.strftime('%m/%d/%y')} is {len(recs)}")
 
+temperature_data = TemperatureData(data)
+recs = temperature_data.get_data_by_area(test_area)
+
+print(f"The house temperature sensor records for area: {test_area} is {len(recs)}")
+print(f"The Maximum temp is {max(recs)} and the minimum temp is {min(recs)}")
+
+recs = temperature_data.get_data_by_date(test_date)
+print(f"The house temperature sensor records for temperature: {test_date.strftime('%m/%d/%y')} is {len(recs)}")
+print(f"The Maximum temp is {max(recs)} and the minimum temp is {min(recs)}")
