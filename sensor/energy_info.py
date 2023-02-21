08532d817d1c39d52f97f3292fd18854bf2eb9b9
Stephen McCormick <stephen.d.mcc@gmail.com>
2023-02-20 23:37:30 -0500

    Module 6 - Energy usage

--- sensor/energy_info.py
+++ sensor/energy_info.py
@@ -12,7 +12,7 @@ class EnergyData(HouseInfo):
 
         return energy
 
-    def _convert_energy(self, data):
+    def _convert_data(self, data):
         recs = []
 
         for rec in data:
@@ -22,11 +22,11 @@ class EnergyData(HouseInfo):
 
     def get_data_by_date(self, rec_date=date.today()):
         recs = super().get_data_by_date("energy_usage", rec_date)
-        return self._convert_energy(recs)
+        return self._convert_data(recs)
 
     def get_data_by_area(self, rec_area=0):
         recs = super().get_data_by_area("energy_usage", rec_area)
-        return self._convert_energy(recs)
+        return self._convert_data(recs)
 
     def calculate_energy_usage(self, data):
         total_energy = sum([field * self.ENERGY_PER_BULB for field in data])
